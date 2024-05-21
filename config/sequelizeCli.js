bb40c320a1a43a51fc575c89865150cac4d87b74
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2024-05-21 19:28:10 -0300

    Postgres

---
+++ config/sequelizeCli.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    databse: 'visioDB_dev',
+    username: 'visioflix',
+    password: 'visioflix'
+  }
+}
